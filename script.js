c4cad0c502014ff321076792dbda46e15222374e
LeeAmsi666 <129909771+LeeAmsi666@users.noreply.github.com>
2024-01-12 13:13:11 +0700

    Menambahkan landing page dengan dua tombol

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('connectBtn').addEventListener('click', function() {
+    alert('Tombol Connect diklik!');
+    // Tambahkan aksi lain yang diinginkan saat tombol Connect diklik
+});
+
+document.getElementById('sendSolanaBtn').addEventListener('click', function() {
+    alert('Tombol Send Solana Coin diklik!');
+    // Tambahkan aksi lain yang diinginkan saat tombol Send Solana Coin diklik
+});
