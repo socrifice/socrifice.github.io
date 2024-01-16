14d8bd5206c089e6ac54f3866ad72f15ad676eae
LeeAmsi666 <129909771+LeeAmsi666@users.noreply.github.com>
2024-01-16 10:45:32 +0700

    Tambahkan konfigurasi Webpack

--- assets/js/components.js
+++
@@ -1,9 +0,0 @@
-const linkComponent = (title, icon_classes, url) => {
-  return `
-    <div>
-        <a href="${url}" target="_blank" class="links , hover">
-        <i class="${icon_classes} links-in"></i>
-        ${title}</a>
-    </div>
-  `
-}
